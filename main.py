18dfaddc384a4570d97f5e47f660969bfa56f276
aiuc209 <dzuiryuomdbaoy@gmail.com>
2026-04-17 23:00:05 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+Filter 2 Royxatdan Faqat 3 Xonali Sonlarni Ajratib Oladigan 
+Topic: FILTER – 2: ��Ro‘yxatdan faqat 3 xonali sonlarni ajratib oladigan filter funksiyasini yozing. Berilgan ro'yxat: roy = [3, 126, 4.21, 701, 99.9, 1001, 100, 7]
+"""
+
+def main():
+    print("Welcome to Filter 2 Royxatdan Faqat 3 Xonali Sonlarni Ajratib Oladigan !")
+    print("Muvaffaqiyatli ishga tushirildi.")
+
+if __name__ == "__main__":
+    main()
